de5139ac923e901f6d8a0c37374788f32d0812d6
Leon Dong <mddq2000@outlook.com>
2024-03-01 11:29:54 +0800

    learning!

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (int argc, char **argv)
+{
+        printf ("Hello github !\n");
+        printf ("I am a hello-world project for github!\n");
+        
+        return 0;
+} /* ----- End of main() ----- */
