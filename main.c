328555c35ae3a39f47392f6b4d153f45de68db87
Leon Dong <mddq2000@outlook.com>
2024-03-01 16:09:24 +0800

    add branch name

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ int main (int argc, char **argv)
 {
         printf ("Hello github !\n");
         printf ("I am a hello-world project for github!\n");
-        
+        printf("This branch is develop!");
+
         return 0;
 } /* ----- End of main() ----- */
